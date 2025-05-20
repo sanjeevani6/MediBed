f406fe18c9680ccc8de249e8ac1c6bcac6a88bc9
sanjeevani6 <jaiswalsanjeevani4@gmail.com>
2025-05-21 02:57:26 +0530

    env updated

--- Backend/middlewares/authMiddleware.js
+++ Backend/middlewares/authMiddleware.js
@@ -1,7 +1,7 @@
 import jwt from "jsonwebtoken";
 
-const JWT_SECRET = process.env.JWT_SECRET || "your_jwt_secret";
-const REFRESH_SECRET = process.env.REFRESH_SECRET || "your_refresh_secret";
+const JWT_SECRET = process.env.JWT_SECRET ;
+const REFRESH_SECRET = process.env.REFRESH_SECRET ;
  
 /**
  * Generates a new Access Token
